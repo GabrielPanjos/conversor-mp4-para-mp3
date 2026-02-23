e49896fb77f07ef4dd4693e8d9782dd7828f56a4
GabrielPanjos <pereiradosanjosgabriel@gmail.com>
2026-02-22 22:13:02 -0300

    feat: converter mp4 para mp3

--- src/middlewares/convertToMp3.js
+++
@@ -1,3 +0,0 @@
-export default async function convertToMp3(mp4) {
-    
-}
--- src/routes/convertToMp3Routes.js
+++ src/routes/convertToMp3Routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import conversor from "../controllers/index.js";
+import Mp3Conversor from "../controllers/Mp3ConversorController.js";
+import convertToMp3 from "../middlewares/convertMp4ToMp3.js";
 
 const routes = express.Router();
 
-routes.post("/conversion", conversor.getMp4Url);
+routes.post("/conversion", Mp3Conversor.getMp4Url, convertToMp3);
 
 export default routes;
