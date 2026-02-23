7662a4b00798a6e48be056ec3af431f92180d161
GabrielPanjos <pereiradosanjosgabriel@gmail.com>
2026-02-23 15:42:31 -0300

    patch: pegar caminho do mp4

--- src/controllers/Mp3ConversorController.js
+++ src/controllers/Mp3ConversorController.js
@@ -1,9 +1,9 @@
 export default class Mp3Conversor {
   static async getMp4Url(req, res, next) {
     try {
-      const { mp4 } = req.query;
+      const { fileMp4 } = req.query;
 
-      req.mp4 = mp4;
+      req.fileMp4 = fileMp4;
 
       await next();
     } catch (err) {
--- src/middlewares/convertMp4ToMp3.js
+++ src/middlewares/convertMp4ToMp3.js
@@ -3,29 +3,22 @@ import path from "path";
 
 export default async function convertToMp3(req, res, next) {
   try {
-    const safeName = path.parse(path.basename(req.mp4)).name;
+    if (!path.basename(req.fileMp4).includes(".mp4")) {
+      throw new Error("Arquivo mp4 não encontrado.");
+    }
 
-    const inputPath = path.join(
-      process.cwd(),
-      "src",
-      "tmp",
-      "uploads",
-      `${safeName}.mp4`,
-    );
-
-    const fileName = path.parse(safeName).name;
-    const outputFile = `${fileName}.mp3`;
+    const outputFile = path.parse(path.basename(req.fileMp4)).name;
 
     const outputPath = path.join(
       process.cwd(),
       "src",
       "tmp",
       "converted",
-      outputFile,
+      outputFile + ".mp3",
     );
 
     await new Promise((resolve, reject) => {
-      Ffmpeg(inputPath)
+      Ffmpeg(req.fileMp4)
         .noVideo()
         .audioBitrate(192)
         .audioFrequency(44100)
@@ -34,10 +27,14 @@ export default async function convertToMp3(req, res, next) {
         .on("end", resolve)
         .on("error", reject);
 
-      res.status(200).json({
-        message: "Arquivo convertido com sucesso!",
-        fileName: outputFile,
-      });
+      if (path.basename(outputPath).includes(".mp3")) {
+        res.status(200).json({
+          message: "Arquivo convertido com sucesso!",
+          fileName: outputFile + ".mp3",
+        });
+      } else {
+        res.status(404).send("Arquivo mp3 não encontrado.");
+      }
     });
   } catch (err) {
     console.error(err);
