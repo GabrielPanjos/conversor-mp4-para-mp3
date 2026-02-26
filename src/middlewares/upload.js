e0dc7be4f90697e3196cfd98f411b2a6bcea2fea
GabrielPanjos <pereiradosanjosgabriel@gmail.com>
2026-02-26 18:16:39 -0300

    nova estrutura da API

---
+++ src/middlewares/upload.js
@@ -0,0 +1,23 @@
+import multer from "multer";
+import path from "path";
+import fs from "fs";
+
+const uploadDir = path.join(process.cwd(), "src", "tmp", "uploads");
+
+fs.mkdirSync(uploadDir, {
+  recursive: true,
+});
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, uploadDir);
+  },
+
+  filename(req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage }).single("file");
+
+export default upload;
